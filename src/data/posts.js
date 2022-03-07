850eaef370a2ac37100580430724e2deb22ff66d
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-08 00:38:58 +0630

    mock data file added

---
+++ src/data/posts.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    id: 'p2',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u2',
+      username: 'jkm',
+      imageUri:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    },
+    description: 'Hello there',
+    songName: 'NF - The search',
+    songImage:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+  {
+    id: 'p1',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'daviddobrik',
+      imageUri:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'NF - The search',
+    songImage:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+  {
+    id: 'p3',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u3',
+      username: 'jkma',
+      imageUri:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    },
+    description: 'Hello there',
+    songName: 'NF - The search',
+    songImage:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+  {
+    id: 'p4',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u4',
+      username: 'daviddobrik1212',
+      imageUri:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'NF - The search',
+    songImage:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+  },
+];
