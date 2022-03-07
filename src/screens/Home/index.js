5bf6bbc8ecdfac2bbf9e5366b067d7174d5f6e33
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-08 00:39:34 +0630

    flatlist added

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,53 +1,21 @@
-import {StyleSheet, View} from 'react-native';
+import {Dimensions, FlatList, View} from 'react-native';
 import React from 'react';
 import Post from '../../components/Post';
-
-const post1 = {
-  id: 'p1',
-  videoUri:
-    'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
-  user: {
-    id: 'u1',
-    username: 'daviddobrik',
-    imageUri:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
-  },
-  description: 'hahahah oh boy @borat',
-  songName: 'NF - The search',
-  songImage:
-    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
-  likes: 123,
-  comments: 12,
-  shares: 44,
-};
-
-const post2 = {
-  id: 'p2',
-  videoUri:
-    'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
-  user: {
-    id: 'u2',
-    username: 'jkm',
-    imageUri:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
-  },
-  description: 'Hello there',
-  songName: 'NF - The search',
-  songImage:
-    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.j-14.com%2Fwp-content%2Fuploads%2F2020%2F05%2FDavid-dobrik-confronts-drunk-stranger.jpeg%3Fresize%3D1000%252C561&f=1&nofb=1',
-  likes: 123,
-  comments: 12,
-  shares: 44,
-};
+import posts from '../../data/posts';
 
 const Home = () => {
   return (
-    <View style={styles.home}>
-      <Post post={post1} />
+    <View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        snapToInterval={Dimensions.get('window').height - 24}
+        snapToAlignment="start"
+        decelerationRate={'fast'}
+        data={posts}
+        renderItem={({item}) => <Post post={item} />}
+      />
     </View>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({});
