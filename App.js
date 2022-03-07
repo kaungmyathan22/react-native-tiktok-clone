edf74f27ace8eaf6125e8016dc9f8af3df70849e
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-07 13:32:45 +0630

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {SafeAreaView} from 'react-native';
+import Home from './src/screens';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
+  );
+};
+
+export default App;
