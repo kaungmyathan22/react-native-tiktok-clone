1ad4aa5aa842b28ced800ca324c73397a8d2a033
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-08 01:36:26 +0630

    height reduced

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -3,7 +3,7 @@ import {Dimensions, StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: Dimensions.get('window').height - 24,
+    height: Dimensions.get('window').height - 76,
   },
   video: {
     position: 'absolute',
